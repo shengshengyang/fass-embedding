582fb0dbc04898c1ca2d7d7820611ea387c962bd
deanyang122 <111415664+deanyang122@users.noreply.github.com>
2023-07-27 16:35:15 +0800

    Modify: remove unuse

--- showVector.py
+++ showVector.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib
 from matplotlib.font_manager import FontProperties
-matplotlib.rcParams['font.sans-serif'] = ['SimHei']  # Set the font to SimHei
 matplotlib.rcParams['axes.unicode_minus'] = False  # Solve the problem that minus sign '-' is displayed as a square
 
 
@@ -22,24 +21,7 @@ data = pd.read_excel('phone2.xlsx', engine='openpyxl')
 # Create a 3D scatter plot of the title vectors
 fig = plt.figure(figsize=(20, 20))
 ax = fig.add_subplot(111, projection='3d')
-# Use a colormap to differentiate data points
-scatter = ax.scatter(title_vectors[:, 0], title_vectors[:, 1], title_vectors[:, 2], c=title_vectors[:, 2], cmap='viridis')
-
-# Add gridlines
-ax.grid(True)
-
-# Set labels for x, y, and z axes
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-
-# Set a title for the graph
-ax.set_title('3D graph')
-
-# Add a colorbar
-fig.colorbar(scatter)
-
-# Rest of your code...
+ax.scatter(title_vectors[:, 0], title_vectors[:, 1], title_vectors[:, 2])
 
 # Set the font properties to SimHei
 font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=14)
